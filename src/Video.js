3d294faf36578a6dee439454ef80b27ee8deabe4
Brandon Powell <brandonpdesigns@gmail.com>
2017-04-04 15:50:25 -0400

    Update Files 3:50pm

--- src/Video.js
+++ src/Video.js
@@ -6,9 +6,23 @@ export default class Songs extends React.Component {
     return (
       <div className="wrapper-song-box">
           <div className="info-tile">
-             <h2 className="left-side">Top Videos</h2>
-             <h2 className="right-side">All Videos</h2>
+             <h2 className="left-side">Top Songs</h2>
+             <h2 className="right-side">All Songs</h2>
           </div>
+
+          <div className="ablum-song-list">
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+              <li><a href="#"><img src="https://static.gigwise.com/gallery/5209864_8262181_JasonDeruloTatGall.jpg" alt="placeholder" /></a></li>
+          </div>
+
+          <div className="arrow-right btn-arrow"></div>
+          <div className="arrow-left btn-arrow"></div>
       </div>
     );
   }
