7ea07de976f31c5a21a6bdd0fbcaf75690179dbb
Brandon Powell <brandonpdesigns@gmail.com>
2017-04-25 16:38:20 -0400

    Update/Add New Files to Project

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import configureStore from '../src/soundcloud/stores/configureStore';
-import * as actions from '../src/soundcloud/actions';
-import Stream from '../src/soundcloud/components/Stream';
+import Nav from '../components/Nav';
+import Hero from '../components/Hero';
+import Player from '../components/Player';
+import About from '../components/About';
+import Gallery from '../components/Gallery';
+import Tracks from '../components/Tracks';
 
-const tracks = [
-  {
-    title: 'Some track'
-  },
-  {
-    title: 'Some other track'
+export default class App extends React.Component {
+  render() {
+    return(
+      <div>
+        <Nav />
+        <Hero />
+        <Player />
+        <About />
+        <Gallery />
+        <Tracks />
+      </div>
+    );
   }
-];
+}
 
-const store = configureStore();
-
-store.dispatch(actions.setTracks(tracks));
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Stream />
-  </Provider>,
-  document.getElementById('container')
-);
+ReactDOM.render(<App />, document.getElementById('container'));
